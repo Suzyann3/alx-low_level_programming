82bcc99c56b7b67e746f7252850c85f3371165de
Suzyann3 <suzyann002@gmail.com>
2022-10-27 16:04:40 -0700

    Strncat c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,8 @@
 /**
  * _strncat - concatenates two strings,
  * @dest: destination.
- * @src: source.* @n: amount of bytes used from src.
+ * @src: source.
+ * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
