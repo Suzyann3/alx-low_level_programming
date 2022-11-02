e7e814aca1a895daaa160a960230909af791e0bd
Suzyann3 <suzyann002@gmail.com>
2022-11-02 11:09:47 -0700

    Recursion

--- main.h
+++ main.h
@@ -1,16 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
