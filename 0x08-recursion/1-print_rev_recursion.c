828f5b904ee3e0f0b55b5faad15e2e4e7d98e7d0
Suzyann3 <suzyann002@gmail.com>
2022-11-03 03:25:13 -0700

    Recursion rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,22 @@
-#include "main.h"
+#inclde "main.h"
+
 /**
-* _puts_recursion - prints a string, followed by a new line.
-* @s: string
-* Return: no return.
+* _print_rev_recursion - Print a string in reverse
+* @s: the string to reverse
+*
+* Return: Nothing.
 */
-void _puts_recursion(char *s)
+
+void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
+if (*s == '\0')
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+
+return;
 }
-else
-_putchar('\n');
+
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
 }
