5b2142e4df83a0cecf59ae781bcf0d2903447159
Suzyann3 <suzyann002@gmail.com>
2022-11-08 14:26:15 -0800

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,6 @@ for (i = 1; i < argc; i++)
 {
 if (is_number(argv[i]) == 1)
 {
-
 printf("Error\n");
 return (1);
 }
@@ -38,3 +37,24 @@ return (0);
 exit(EXIT_SUCCESS);
 }
 
+/**
+* is_number - checks if a character is a number
+* @str: param to check
+* Return: 0 if true else 1
+*/
+
+int is_number(char *str)
+{
+int i = 0;
+
+if (str[0] == '-')
+i = 1;
+
+for (; str[i]; i++)
+{
+if (*(str + i) < '0' || *(str + i) > '9')
+return (1);
+}
+
+return (0);
+}
